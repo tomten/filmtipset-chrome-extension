5a5360b7c4f48f6a047afbd5202569455e37c57a
tomten <github@ul7.info>
2013-10-08 11:28:27 -0700

    4spaces instead of tabs

--- imdb.js
+++ imdb.js
@@ -2,24 +2,24 @@
 
 // constructor
 function Imdb(href){
-	this.href = href;
-	this.common = new Common();
-	}
+    this.href = href;
+    this.common = new Common();
+    }
 
 Imdb.prototype.init = function(){
-	var imdbId = this.common.getImdbIdFromUrl(this.href);
-	if (imdbId) {
-		chrome.extension.sendRequest({ 
-				action: "activateImdbPage", 
-				imdbId: imdbId 
-				});
-		chrome.extension.sendRequest({ 
-				action: "track", 
-				trackCategory: "imdb", 
-				trackAction: "showGradeOnImdb" 
-				});
-		}
-	};
+    var imdbId = this.common.getImdbIdFromUrl(this.href);
+    if (imdbId) {
+        chrome.extension.sendRequest({ 
+                action: "activateImdbPage", 
+                imdbId: imdbId 
+                });
+        chrome.extension.sendRequest({ 
+                action: "track", 
+                trackCategory: "imdb", 
+                trackAction: "showGradeOnImdb" 
+                });
+        }
+    };
 
 var imdb = new Imdb(window.location.href);
 imdb.init();
