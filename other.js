3d89eed4ab2ca3f6111614d3726513f8018d042f
tomten <github@ul7.info>
2013-10-21 10:01:46 -0700

    don't run other.js on imdb

--- other.js
+++ other.js
@@ -1,4 +1,6 @@
 $(function(){
-    var links = new FilmtipsetExtension.Links($);
-    links.processLinks();
+    if (location.hostname.indexOf("imdb") === -1) { // Don't run this on IMDB
+        var links = new FilmtipsetExtension.Links($);
+        links.processLinks();
+        }
     });
